705a380bf663b208a8bff7733ee96197d8b376d9
SmachuloTetiana <tanichk@ukr.net>
2016-06-08 09:06:41 +0000

    add file

---
+++ js/animation.js
@@ -0,0 +1,30 @@
+  function testAnim(x) {
+    $('#animationSandbox').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass();
+    });
+  };
+
+  $(document).ready(function(){
+    $('.js--triggerAnimation').click(function(){
+      var anim = $('.js--animations').val();
+      testAnim(anim);
+    });
+
+    $('.js--animations').change(function(){
+      var anim = $(this).val();
+      testAnim(anim);
+    });
+	
+	$("#modal, #modal2").addClass("animated slideInDown");
+	
+	$(".modal-but-two").on("click", function(){
+		$("#modal").arcticmodal("close");
+	})
+		
+
+  });
+
+/***********bounceInLeft**************/
+
+
+  
