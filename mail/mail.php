575697be33ec3840a3ffdcb79ac0d79b972d4508
SmachuloTetiana <tanichk@ukr.net>
2016-06-13 18:29:47 +0000

    add file

--- mail/mail.php
+++ mail/mail.php
@@ -10,6 +10,8 @@ include "mail.class.php";
 	$m->Subject("Сайт Foto-clip");
 	$m->Body("<br/>
 	".(!empty($_POST['name'])?"<b>Ім'я</b>: ".$_POST['name']."<br/>":"")."
+	".(!empty($_POST['file'])?"<b>Ім'я</b>: ".$_POST['file']."<br/>":"")."
+	".(!empty($_POST['recipe'])?"<b>Ім'я</b>: ".$_POST['recipe']."<br/>":"")."
 	".(!empty($_POST['email'])?"<b>E-mail</b>: ".$_POST['email']."<br/>":"")."
 	".(!empty($_POST['comment'])?"<b>Запитання</b>: ".$_POST['comment']."<br/>":"")."");
 	$m->Send();
